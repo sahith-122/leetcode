6fefe69d243b92f492ad6aac701970c4513952d4
sahith-122 <gsahith143@gmail.com>
2026-09-01 23:09:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+class Solution {
+public:
+    int search(std::vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        
+        while (low <= high) {
+           
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid] == target) {
+                return mid; 
+            } 
+            else if (nums[mid] < target) {
+                low = mid + 1; 
+            } 
+            else {
+                high = mid - 1; 
+            }
+        }
+        
+        return -1; 
+    }
+};
