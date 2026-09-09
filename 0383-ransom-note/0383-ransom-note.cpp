9d1326346d2f06b61953fdca4dd99494d09ffc6d
sahith-122 <gsahith143@gmail.com>
2026-09-09 10:41:53 +0530

    Time: 0 ms (100%), Space: 11.7 MB (89.26%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int count[26]={0};
+        for(char c : magazine) {
+    count[c - 'a']++;
+}
+for(char c : ransomNote) {
+    if(count[c - 'a'] == 0)
+        return false;
+
+    count[c - 'a']--;
+}
+return true;
+
+            
+        }
+        
+    
+};
